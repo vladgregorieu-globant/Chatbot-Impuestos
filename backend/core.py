d5edeca89e5cf33388ad99a02ebc0962433b6ade
vladgregorieu-globant <vladimir.gregorieu@globant.com>
2023-09-08 12:23:32 -0300

    Create tax-code chatbot advisor

---
+++ backend/core.py
@@ -0,0 +1,37 @@
+import os
+from typing import Any, Dict, List
+
+from langchain.embeddings import HuggingFaceHubEmbeddings
+from langchain import HuggingFaceHub
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationalRetrievalChain
+from langchain.vectorstores import Pinecone
+import pinecone
+
+
+pinecone.init(
+    api_key=os.environ["PINECONE_API_KEY"],
+    environment=os.environ["PINECONE_ENVIRONMENT_REGION"],
+)
+
+INDEX_NAME = "internal-revenue-code-index"
+
+
+def run_llm(query: str, chat_history: List[Dict[str, Any]] = []):
+    embeddings = HuggingFaceHubEmbeddings()
+    docsearch = Pinecone.from_existing_index(
+        embedding=embeddings,
+        index_name=INDEX_NAME,
+    )
+    repo_id="google/flan-t5-xxl"
+    chat = HuggingFaceHub(
+    repo_id=repo_id, model_kwargs={"temperature": 0.5, "max_length": 64}
+    )
+
+    qa = ConversationalRetrievalChain.from_llm(
+        llm=chat, retriever=docsearch.as_retriever(), return_source_documents=True
+    )
+    return qa({"question": query, "chat_history": chat_history})
+
+if __name__ == "__main__":
+    print(run_llm(query="What if I changed my name?"))
---
+++ ingest.py
@@ -0,0 +1,31 @@
+import os
+
+from langchain.document_loaders import PDFMinerLoader
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Pinecone
+import pinecone
+
+pinecone.init(
+    api_key=os.environ["PINECONE_API_KEY"],
+    environment=os.environ["PINECONE_ENVIRONMENT_REGION"],
+)
+INDEX_NAME = "internal-revenue-code-index"
+
+def ingest_docs()->None:
+    loader= PDFMinerLoader("tax-code/i1040gi.pdf")
+    raw_documents = loader.load()
+    print(f"loaded {len(raw_documents)} documents")
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=400, chunk_overlap=50, separators=["\n\n", "\n", " ", "", "\xa0"]
+    )
+    documents = text_splitter.split_documents(raw_documents)
+    print(f"Splitted into {len(documents)} chunks")
+    embeddings = HuggingFaceEmbeddings()
+    print(f"Going to add {len(documents)} to Pinecone")
+    Pinecone.from_documents(documents, embeddings, index_name=INDEX_NAME)
+    print("****Loading to vector store done ***")
+
+
+if __name__ == "__main__":
+    ingest_docs()
